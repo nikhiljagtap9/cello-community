cb1f7fa21372f5cf0483f5e678a12d6850105f4b
unknown <girishbhumkar5@gmail.com>
2025-09-22 11:44:19 +0530

    Error solved

---
+++ imp.php
@@ -0,0 +1,19 @@
+admin@example.com
+admin123
+
+user@gmail.com
+admin123
+
+
+https://github.com/nikhiljagtap9/cello-community
+
+*************
+git commit -am "Added Freelancers"
+git branch
+git add . 
+git commit -m "Added Freelances" 
+git push origin girish_dev
+
+ON OUR SERVER 
+git status
+git pull origin master
